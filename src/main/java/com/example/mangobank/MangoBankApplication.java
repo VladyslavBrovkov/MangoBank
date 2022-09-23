5f3eb211645535cb573ce68dc2d7ef0ab92beff2
Vladyslav Brovkov <brovkov777@gmail.com>
2022-09-23 13:50:55 +0300

    Project Creation

---
+++ src/main/java/com/example/mangobank/MangoBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.mangobank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangoBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangoBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mangobank/MangoBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mangobank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangoBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
