9ae8b1d6ae5c7aab121b4aa75b03b2b9523d6f87
Vladyslav Brovkov <brovkov777@gmail.com>
2022-10-04 14:13:54 +0300

    CORS added

--- src/main/java/com/example/mangobank/security/SecurityConfig.java
+++ src/main/java/com/example/mangobank/security/SecurityConfig.java
@@ -10,6 +10,9 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.List;
 
@@ -21,15 +24,28 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.csrf().disable()
+                .cors().and()
                 .authorizeRequests()
                 .antMatchers("/login", "/user/**").permitAll()
                 .anyRequest()
                 .fullyAuthenticated()
                 .and()
                 .formLogin()
+                .loginProcessingUrl("/perform_login")
                 .defaultSuccessUrl("/homepage", true)
                 .and()
                 .logout().and().build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+            }
+        };
     }
 
     @Bean
