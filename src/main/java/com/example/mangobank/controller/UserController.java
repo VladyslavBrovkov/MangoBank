53ac3630828bc3f22557a190f1f3299fc026b74f
Vladyslav Brovkov <brovkov777@gmail.com>
2022-11-02 12:35:48 +0200

    localhost added to origin userController

--- src/main/java/com/example/mangobank/controller/UserController.java
+++ src/main/java/com/example/mangobank/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/user")
 public class UserController {
     private final UserServiceImpl service;
