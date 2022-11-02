bed864b404d673287bc29374d0f6531db1442fdf
Vladyslav Brovkov <brovkov777@gmail.com>
2022-11-02 16:45:10 +0200

    localhost added to origin loginController

--- src/main/java/com/example/mangobank/controller/LoginJwtController.java
+++ src/main/java/com/example/mangobank/controller/LoginJwtController.java
@@ -14,7 +14,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*")
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 public class LoginJwtController {
 
     @Autowired
--- src/main/java/com/example/mangobank/controller/UserController.java
+++ src/main/java/com/example/mangobank/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*")
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 @RequestMapping("/user")
 public class UserController {
     private final UserServiceImpl service;
